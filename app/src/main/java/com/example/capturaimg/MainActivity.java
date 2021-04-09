d611452dc09415af41d4d91bf252fe6816cc1fe3
Julii4nn2 <56120552+Julii4nn2@users.noreply.github.com>
2021-04-08 21:06:48 -0300

    Laptop

--- app/src/main/java/com/example/capturaimg/MainActivity.java
+++ app/src/main/java/com/example/capturaimg/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.capturaimg;
 
+import android.Manifest;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.MediaStore;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.File;
@@ -19,17 +23,23 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imagen1;
     private EditText et1;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //solicito los permisos para camara y almacenamiento
+        requestPermissions(new String[]{ Manifest.permission.CAMERA }, 1);
+        requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
 
+        setContentView(R.layout.activity_main);
         imagen1=(ImageView)findViewById(R.id.imageView);
         et1=(EditText)findViewById(R.id.editText);
     }
 
     public void tomarFoto(View v) {
         Intent intento1 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intento1,0);
         File foto = new File(getExternalFilesDir(null), et1.getText().toString());
         intento1.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(foto));
         startActivity(intento1);
@@ -43,5 +53,12 @@ public class MainActivity extends AppCompatActivity {
     public void ver(View v) {
         Intent intento1=new Intent(this,Actividad2.class);
         startActivity(intento1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        imagen1.setImageBitmap(bitmap);
     }
 }
